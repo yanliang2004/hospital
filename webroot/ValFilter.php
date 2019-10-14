5fcd353bbabb9bb53234f02e6beb720dede4c3e8
yl <yanliang2004@qq.com>
2019-10-14 14:50:04 +0800

    git ignore

--- webroot/ValFilter.php
+++ webroot/ValFilter.php
@@ -4,6 +4,35 @@
  * 不仅验证，还转换数据，
  * deptId = '12' -> deptId = 12
  * 
+ * 利用现成的 php Filter 函数
+ * 
+ * $v = new ValFilter([
+ *     'name' => [
+ *          FILTER_SANITIZE_STRING,
+ *          [ 'empty' => '姓名不能为空' ]
+ *     ],
+ *     
+ *      'dept' => [
+ *          FILTER_VALIDATE_INT,
+ *          [ 
+ *              'empty' => '科室id不能为空', 
+ *              'invalid' => '科室id不对' 
+ *          ]
+ *      ]
+ * ]);
+ * 
+ * 
+ * 自带两种结果： false：验证失败， null：数据为空
+ * An array value will be FALSE if the filter fails, 
+ * or NULL if the variable is not set.
+ * 
+ * $v->validate($data);   // false: invalid
+ * 
+ * $v->errMsgs   // error msgs
+ * 
+ * 
+ * 
+ * 
  * */
 
 class ValFilter
@@ -33,7 +62,8 @@ class ValFilter
 
         array_walk($this->result, function ($value, $key) {
             if (!$value) {
-                $this->errMsgs[$key] = $this->opt[$key][1][null === $value ? 'empty' : 'invalid'];
+                $this->errMsgs[$key] = 
+                    $this->opt[$key][1][null === $value ? 'empty' : 'invalid'];
             }
         });
 
@@ -52,12 +82,12 @@ function testValFilter() {
     $filter = new ValFilter([
         'uname' => [FILTER_SANITIZE_STRING, ['empty' => '登录名不能为空']],
         'name' => [FILTER_SANITIZE_STRING, ['empty' => '姓名不能为空']],
-        'deptId' => [FILTER_VALIDATE_INT, ['empty' => '科室不能为空', 'invalid' => '科室id非法']]
+        'dept' => [FILTER_VALIDATE_INT, ['empty' => '科室不能为空', 'invalid' => '科室id非法']]
     ]);
 
     $data = $filter->validate([
         'uname'=>'wangl',
-        'deptId' => '12',
+        'dept' => '1.2',
         'name' => 'yl'
     ]);
 
@@ -70,4 +100,4 @@ function testValFilter() {
 
 }
 
-// testValFilter();
+testValFilter();
